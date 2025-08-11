f59d453b37cbb675cbaab86935ebc3a606bad1a9
satheeshraj05 <satheeshraj333@gmail.com>
2025-08-11 13:26:02 +0530

    added experience

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -2,24 +2,24 @@ export const experienceData = [
     
     {
         id: 1,
-        company: 'JUSTFRAMEZ',
-        jobtitle: 'Web Developer',
-        startYear: 'Aug, 2024',
-        endYear: 'Current'
+        company: 'Clar Technologies',
+        jobtitle: 'Software Developer',
+        startYear: 'Apr 2025 ',
+        endYear: ' Current'
     },
     {
         id: 1,
         company: 'EDUFIED',
         jobtitle: 'Frontend Developer Developer',
-        startYear: 'Nov, 2023',
-        endYear: 'July, 2024'
+        startYear: 'Dec 2023 ',
+        endYear: ' July 2024'
     },
     {
         id: 3,
         company: 'LUMOS IND',
         jobtitle: 'Research Associate',
-        startYear: 'Aug, 2020 ',
-        endYear: 'Jan, 2021'
+        startYear: 'Aug 2020 ',
+        endYear: ' Jan 2021'
     },
     
     
